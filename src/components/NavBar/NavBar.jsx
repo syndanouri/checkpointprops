e4a021fc8bdd497af87ede6f0e25928d65939f8c
syndanouri <sinda.nouri@esprit.tn>
2023-02-28 23:22:14 +0100

    first commit

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 import { Container,Navbar } from 'react-bootstrap'
 const NavBar = () => {
   return (
-    <div><Navbar bg="dark" variant="dark">
+    <div>
+      <Navbar bg="dark" variant="dark">
         <Container>
           <Navbar.Brand href="#home">
             <img
               alt=""
-              src="/logo.svg"
-              width="30"
-              height="30"
+              src="https://tse2.mm.bing.net/th?id=OIP.6as4enhVvJer57ylHqTBzAAAAA&pid=Api&P=0"
+              width="40"
+              height="40"
               className="d-inline-block align-top"
-            />{' '}
+            />{" "}
             My shop
           </Navbar.Brand>
         </Container>
-      </Navbar></div>
-  )
+      </Navbar>
+    </div>
+  );
 }
 
 export default NavBar
